b8f4646cc9caba1dd9788a2070778e04c6e9eaa2
Deyr0 <83130116+Deyr0@users.noreply.github.com>
2022-05-29 23:15:33 +0300

    Add files via upload

---
+++ server/ClientObject.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+    
+namespace ChatServer
+{
+    class ClientObject
+    {
+        protected internal string Id { get; private set; }
+        protected internal NetworkStream Stream { get; private set; }
+        string userName;
+        TcpClient client;
+        ServerObject server; // объект сервера
+       protected internal bool first;
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+        {
+            Id = Guid.NewGuid().ToString();
+            client = tcpClient;
+            server = serverObject;
+                         first= true;
+            serverObject.AddConnection(this);
+
+        }
+
+        public void Process()
+        {
+            try
+            {
+                Stream = client.GetStream();
+                // получаем имя пользователя
+                string message = GetMessage();
+                userName = message;
+                
+                message = userName + " вошел в чат";
+                // посылаем сообщение о входе в чат всем подключенным пользователям
+                //server.BroadcastMessage(message, this.Id);
+                Console.WriteLine(message);
+                
+                // в бесконечном цикле получаем сообщения от клиента
+                while (true)
+                {
+                    try
+                    {
+                        byte[] mess = Getmessage();
+                        if (mess.Length != 0)
+                        {
+                            Console.WriteLine("new message from {0}", userName);
+                            string m = Encoding.Unicode.GetString(mess, 0, mess.Length);
+                            Console.WriteLine(m);
+                            server.BroadcastMessage(mess, this.Id);
+                        }
+                        
+                    }
+                    catch
+                    {
+                        message = String.Format("{0}: покинул чат", userName);
+                        Console.WriteLine(message);
+                        //server.BroadcastMessage(message, this.Id);
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // в случае выхода из цикла закрываем ресурсы
+                server.RemoveConnection(this.Id);
+                Close();
+            }
+        }
+
+        // чтение входящего сообщения и преобразование в строку
+        private string GetMessage()
+        {
+            byte[] data = new byte[64]; // буфер для получаемых данных
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            do
+            {
+                bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (Stream.DataAvailable) ;
+
+            return builder.ToString();
+        }
+
+        private byte[] Getmessage()
+        {
+            byte[] data = new byte[64]; // буфер для получаемых данных
+            MemoryStream stream = new MemoryStream();
+            do
+            {
+                Stream.Read(data, 0, data.Length);
+                stream.Append(data);
+
+            }
+            while (Stream.DataAvailable);
+            byte[] bytes = stream.ToArray();
+            
+            return bytes;
+        }
+
+        // закрытие подключения
+        protected internal void Close()
+        {
+            if (Stream != null)
+                Stream.Close();
+            if (client != null)
+                client.Close();
+        }
+    }
+    public static class MemoryStreamExtensions
+    {
+        public static void Append(this MemoryStream stream, byte value)
+        {
+            stream.Append(new[] { value });
+        }
+
+        public static void Append(this MemoryStream stream, byte[] values)
+        {
+            stream.Write(values, 0, values.Length);
+        }
+    }
+}
---
+++ server/ServerObject.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    class ServerObject
+    {
+        static TcpListener tcpListener; // сервер для прослушивания
+        protected List<ClientObject> clients = new List<ClientObject>(); // все подключения
+
+        protected internal void AddConnection(ClientObject clientObject)
+        {
+            clients.Add(clientObject);
+
+        }
+        protected internal void RemoveConnection(string id)
+        {
+            // получаем по id закрытое подключение
+            ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+            // и удаляем его из списка подключений
+            if (client != null)
+                clients.Remove(client);
+        }
+        // прослушивание входящих подключений
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                    ClientObject clientObject = new ClientObject(tcpClient, this);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                    Task.Delay(1000);
+                    Console.WriteLine(clients.Count);
+                    if (clients.Count == 1)
+                    {
+                        byte[] vs = BitConverter.GetBytes(1);
+                        clients[0].Stream.Write(vs, 0, vs.Length);
+                    }
+                    else
+                    {
+                        byte[] vs = BitConverter.GetBytes(10);
+                        Task.Delay(1000);
+                        clients[clients.Count - 1].Stream.Write(vs, 0, vs.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        // трансляция сообщения подключенным клиентам
+        
+
+        protected internal void BroadcastMessage(byte[] message, string id)
+        {
+            
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].Id != id) // если id клиента не равно id отправляющего
+                {
+                clients[i].Stream.Write(message, 0, message.Length); //передача данных
+                    Console.WriteLine("отправлено");
+                }
+            }
+        }
+
+       
+
+        // отключение всех клиентов
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop(); //остановка сервера
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close(); //отключение клиента
+            }
+            Environment.Exit(0); //завершение процесса
+        }
+    }
+}
